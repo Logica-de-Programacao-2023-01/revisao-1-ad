8ca70d747fb80dec8b2f514086e29cd0d830e3df
Davi Santos Pacini <davi.pacini@dinfor.uniceub>
2023-04-14 10:43:17 -0300

    q1

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,30 @@
 package q1
 
+import "fmt"
+
 func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
-	// Implemente sua solução aqui
+	var soma float64
+	var z float64
+	var desconto float64
+	z = 0
+
+	for i := 0; i < len(purchaseHistory); i++ {
+		soma = soma + purchaseHistory[i]
+		z++
+	}
+	media := soma / z
+
+	if currentPurchase <= 0 {
+		return 0, fmt.Errorf("Valor da compra atual é invalido")
+	}
+
+	if media > 1000 {
+		desconto = 20 / 100 * currentPurchase
+		return desconto, nil
+	} else if len(purchaseHistory) == 0 {
+		desconto = 10 / 100 * currentPurchase
+		return desconto, nil
+	}
+
 	return 0, nil
 }
